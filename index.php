fa9ea25f6b63eaeae5400794715c0c41e0a3f16a
Unknown <d231100@edu.rocwb.nl>
2018-02-22 10:01:37 +0100

    some image changes

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
         <h1>Release The CRACKEN!!!</h1>
         <img src="img/gitkraken_stickers-r514220fb582c40a88e313a74d7660fe6_v9i40_8byvr_630.jpg" alt="git kraken logo">
         <img src="img/git_kraken_overvieuw.png" alt="Git Kracken overvieuw">
+        <img src="img/kraken_v2_by_elmisa.jpg" alt="ART">
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam corporis, cum deleniti dicta ea, eaque eligendi explicabo in ipsam ipsum minus nulla odit praesentium quaerat sequi sint totam ullam unde.</p>
     </div>
 </body>
