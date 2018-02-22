fb517d07b1dc45ba0de44e3d9f7ae5bb64e35fd1
Unknown <d231100@edu.rocwb.nl>
2018-02-22 10:03:48 +0100

    added 5thimage

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
         <img src="img/gitkraken_stickers-r514220fb582c40a88e313a74d7660fe6_v9i40_8byvr_630.jpg" alt="git kraken logo">
         <img src="img/git_kraken_overvieuw.png" alt="Git Kracken overvieuw">
         <img src="img/kraken_v2_by_elmisa.jpg" alt="ART">
+        <img src="img/kraken__version_2__by_chris_garrett-d51wltk.jpg" alt="Rule1">
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam corporis, cum deleniti dicta ea, eaque eligendi explicabo in ipsam ipsum minus nulla odit praesentium quaerat sequi sint totam ullam unde.</p>
     </div>
 </body>
