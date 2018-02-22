c57f49c8c62701440b0084bcf45071047a45b1ca
Unknown <d231100@edu.rocwb.nl>
2018-02-22 09:51:01 +0100

    Added image folder

--- index.php
+++ index.php
@@ -10,6 +10,9 @@
 <body>
     <div class="wrapper">
         <h1>Release The CRACKEN!!!</h1>
+        <img src="img/gitkraken_stickers-r514220fb582c40a88e313a74d7660fe6_v9i40_8byvr_630.jpg" alt="git kraken logo">
+        <img src="img/git_kraken_overvieuw.png" alt="Git Kracken overvieuw">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam corporis, cum deleniti dicta ea, eaque eligendi explicabo in ipsam ipsum minus nulla odit praesentium quaerat sequi sint totam ullam unde.</p>
     </div>
 </body>
 </html>
